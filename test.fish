78a1895b54bd775bb51c39a82865ea92ea0877f4
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-11-27 23:37:54 +0000

    add: improved fish test commands

--- test.fish
+++ test.fish
@@ -5,17 +5,49 @@
 set IN ./testdata/*.in
 set OUT ./testdata/*.out
 set TESTS (count $IN)
+set SHOW_DIFF true
 
 # Compiling option
-set CPP "g++ -O2 -w -lm" # C++
+set CPP g++ -O2 -w -lm # C++
 
-function execute 
-    for i in $TESTS
-        echo $i
-        echo ./tests/$i.out
-    end 
+function compile -a SOURCE
+    echo "================COMPILE================"
+    $CPP $SOURCE
+    set_color blue 
+    echo "[COMPILE] 💨 Starting..."
+    set_color green
+    echo "[COMPILE] ⭕ $SOURCE compiled!"
+    set_color normal
 end 
 
+
+function execute 
+    echo "=================TESTS================="
+    for i in $TESTS
+        ./a.out < ./testdata/$i.in > ./testdata/$i.myout
+        set result (diff ./testdata/$i.out ./testdata/$i.myout)
+        show_result "$result" $i
+    end 
+end
+
+function show_result -a result testnum 
+    if test -z "$result" 
+        set_color green    
+        echo "[TEST::$testnum] :: ✔️ PASSED!"
+    else if $SHOW_DIFF
+        set_color red 
+        echo "[TEST::$testnum] :: ❌ WRONG ANSWER!"
+        set_color normal
+        echo $result
+    else 
+        set_color red 
+        echo "[TEST::$testnum] :: ❌ WRONG ANSWER!"
+    end 
+    set_color normal
+end 
+
+
+# MAIN ==================================================================
 if test (count $argv) -lt 1
   echo "ℹ️ Usage: ./test <source-code>"
 else if not test -d "./testdata"
@@ -23,6 +55,7 @@ else if not test -d "./testdata"
 else if test (count $IN) -ne (count $OUT)
     echo "⛔ Number of outputs must be equal to the number of inputs"
 else 
+    compile $argv[1]
     execute
 end
 
